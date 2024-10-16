ecf13dbb85c27a7203acd190cfaf98c24959f8e7
Justin Schulz <justin.schulz@gmail.com>
2024-10-16 14:56:08 -0400

    Fix chip clickability

--- lib/main.dart
+++ lib/main.dart
@@ -36,98 +36,98 @@ class EmotionCategory {
   final List<String> emotions;
 }
 
+final List<EmotionCategory> emotionCategories = [
+  EmotionCategory(
+    name: 'bad',
+    color: const Color.fromRGBO(39, 186, 158, 1),
+    emoji: '(◕︵◕)',
+    emotions: [
+      'tired',
+      'stressed',
+      'busy',
+      'bored',
+    ],
+  ),
+  EmotionCategory(
+      name: 'fearful',
+      color: const Color.fromRGBO(22, 163, 186, 1),
+      emoji: '(●﹏●)',
+      emotions: [
+        'scared',
+        'anxious',
+        'insecure',
+        'weak',
+        'rejected',
+        'threatened',
+      ]),
+  EmotionCategory(
+      name: 'angry',
+      color: const Color.fromRGBO(117, 117, 180, 1),
+      emoji: '(ಠ益ಠ)',
+      emotions: [
+        'let down',
+        'humiliated',
+        'insecure',
+        'weak',
+        'rejected',
+        'threatened',
+      ]),
+  EmotionCategory(
+      name: 'disgusted',
+      color: const Color.fromRGBO(172, 124, 181, 1),
+      emoji: '(๑-﹏-๑)',
+      emotions: [
+        'disapproving',
+        'disappointed',
+        'awful',
+        'repelled',
+      ]),
+  EmotionCategory(
+      name: 'sad',
+      color: const Color.fromRGBO(207, 125, 137, 1),
+      emoji: '(ಥ_ಥ)',
+      emotions: [
+        'hurt',
+        'depressed',
+        'guilty',
+        'despair',
+        'vulnerable',
+        'lonely',
+      ]),
+  EmotionCategory(
+      name: 'happy',
+      color: const Color.fromRGBO(226, 135, 109, 1),
+      emoji: '(◕‿◕✿)',
+      emotions: [
+        'optimistic',
+        'trusting',
+        'peaceful',
+        'powerful',
+        'accepted',
+        'proud',
+        'interested',
+        'content',
+        'playful',
+      ]),
+  EmotionCategory(
+    name: 'surprised',
+    color: const Color.fromRGBO(231, 165, 77, 1),
+    emoji: '( ◐ o ◑ )',
+    emotions: [
+      'excited',
+      'amazed',
+      'confused',
+      'startled',
+    ],
+  ),
+];
+
 class _ListWheelScrollViewAppState extends State<ListWheelScrollViewApp> {
   var _focusedIndex = 0;
-  Set<String> selectedEmotions = {};
+  List<String> _selectedEmotions = [];
 
   @override
   Widget build(BuildContext context) {
-    final List<EmotionCategory> emotionCategories = [
-      EmotionCategory(
-        name: 'bad',
-        color: const Color.fromRGBO(39, 186, 158, 1),
-        emoji: '(◕︵◕)',
-        emotions: [
-          'tired',
-          'stressed',
-          'busy',
-          'bored',
-        ],
-      ),
-      EmotionCategory(
-          name: 'fearful',
-          color: const Color.fromRGBO(22, 163, 186, 1),
-          emoji: '(●﹏●)',
-          emotions: [
-            'scared',
-            'anxious',
-            'insecure',
-            'weak',
-            'rejected',
-            'threatened',
-          ]),
-      EmotionCategory(
-          name: 'angry',
-          color: const Color.fromRGBO(117, 117, 180, 1),
-          emoji: '(ಠ益ಠ)',
-          emotions: [
-            'let down',
-            'humiliated',
-            'insecure',
-            'weak',
-            'rejected',
-            'threatened',
-          ]),
-      EmotionCategory(
-          name: 'disgusted',
-          color: const Color.fromRGBO(172, 124, 181, 1),
-          emoji: '(๑-﹏-๑)',
-          emotions: [
-            'disapproving',
-            'disappointed',
-            'awful',
-            'repelled',
-          ]),
-      EmotionCategory(
-          name: 'sad',
-          color: const Color.fromRGBO(207, 125, 137, 1),
-          emoji: '(ಥ_ಥ)',
-          emotions: [
-            'hurt',
-            'depressed',
-            'guilty',
-            'despair',
-            'vulnerable',
-            'lonely',
-          ]),
-      EmotionCategory(
-          name: 'happy',
-          color: const Color.fromRGBO(226, 135, 109, 1),
-          emoji: '(◕‿◕✿)',
-          emotions: [
-            'optimistic',
-            'trusting',
-            'peaceful',
-            'powerful',
-            'accepted',
-            'proud',
-            'interested',
-            'content',
-            'playful',
-          ]),
-      EmotionCategory(
-        name: 'surprised',
-        color: const Color.fromRGBO(231, 165, 77, 1),
-        emoji: '( ◐ o ◑ )',
-        emotions: [
-          'excited',
-          'amazed',
-          'confused',
-          'startled',
-        ],
-      ),
-    ];
-
     final items = Iterable<int>.generate(emotionCategories.length).map((idx) {
       final dataKey = GlobalKey();
 
@@ -158,78 +158,106 @@ class _ListWheelScrollViewAppState extends State<ListWheelScrollViewApp> {
           Scrollable.ensureVisible(dataKey.currentContext!,
               curve: Curves.easeIn, duration: Durations.medium3);
           showModalBottomSheet(
-              context: context,
-              builder: (context) {
-                return StatefulBuilder(builder: (context, setState) {
-                  void handleEmotionSelected(String emotion) {
-                    if (selectedEmotions.contains(emotion)) {
-                      selectedEmotions.remove(emotion);
-                    } else {
-                      selectedEmotions.add(emotion);
-                    }
+            context: context,
+            builder: (context) {
+              return StatefulBuilder(builder: (context, setState) {
+                void handleEmotionSelected(String emotion) {
+                  if (_selectedEmotions.contains(emotion)) {
                     setState(() {
-                      selectedEmotions = selectedEmotions;
+                      _selectedEmotions = _selectedEmotions
+                          .where(
+                              (selectedEmotion) => selectedEmotion != emotion)
+                          .toList();
+                    });
+                  } else {
+                    setState(() {
+                      _selectedEmotions = [
+                        ..._selectedEmotions,
+                        emotion,
+                      ];
                     });
                   }
+                }
 
-                  return Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Column(
-                        // mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          const SizedBox(height: 20),
-                          Text(
-                            emotionCategories[idx].name.toUpperCase(),
-                            style: const TextStyle(
-                              fontWeight: FontWeight.w600,
-                            ),
+                return Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        Text(
+                          emotionCategories[idx].name.toUpperCase(),
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w600,
                           ),
-                          const SizedBox(height: 20),
-                          Wrap(
-                            spacing: 2.0,
-                            alignment: WrapAlignment.spaceEvenly,
-                            children:
-                                emotionCategories[idx].emotions.map((emotion) {
-                              if (selectedEmotions.contains(emotion)) {
-                                return FilledButton(
-                                  onPressed: () =>
-                                      handleEmotionSelected(emotion),
-                                  child: Text(emotion),
-                                );
-                              }
-                              return OutlinedButton(
+                        ),
+                        const SizedBox(height: 20),
+                        Wrap(
+                          spacing: 2.0,
+                          alignment: WrapAlignment.spaceEvenly,
+                          children:
+                              emotionCategories[idx].emotions.map((emotion) {
+                            if (_selectedEmotions.contains(emotion)) {
+                              return FilledButton(
                                 onPressed: () => handleEmotionSelected(emotion),
-                                style: const ButtonStyle(),
                                 child: Text(emotion),
                               );
-                            }).toList(),
-                          )
-                        ],
-                      ));
-                });
+                            }
+                            return OutlinedButton(
+                              onPressed: () => handleEmotionSelected(emotion),
+                              style: const ButtonStyle(),
+                              child: Text(emotion),
+                            );
+                          }).toList(),
+                        )
+                      ],
+                    ));
               });
+            },
+          );
         },
       );
     }).toList();
 
     return Scaffold(
       appBar: AppBar(
-        title: const Text('ListWheelScrollView Sample'),
+        title: const Text('MNTL FLOSS'),
+        actions: const [
+          // TODO: Add ability to record an emotions log
+        ],
       ),
-      body: Center(
-        child: ListWheelScrollView.useDelegate(
-          itemExtent: 150,
-          physics: const FixedExtentScrollPhysics(),
-          overAndUnderCenterOpacity: 0.5,
-          childDelegate: ListWheelChildLoopingListDelegate(
-            children: items,
+      body: Stack(
+        children: [
+          ListWheelScrollView.useDelegate(
+            itemExtent: 150,
+            physics: const FixedExtentScrollPhysics(),
+            overAndUnderCenterOpacity: 0.5,
+            childDelegate: ListWheelChildLoopingListDelegate(
+              children: items,
+            ),
+            onSelectedItemChanged: (index) {
+              setState(() {
+                _focusedIndex = index;
+              });
+            },
           ),
-          onSelectedItemChanged: (index) {
-            setState(() {
-              _focusedIndex = index;
-            });
-          },
-        ),
+          Wrap(
+            direction: Axis.horizontal,
+            children: _selectedEmotions
+                .map((emotion) => Container(
+                      child: InputChip(
+                        label: Text(emotion),
+                        onDeleted: () {
+                          setState(() {
+                            _selectedEmotions = _selectedEmotions
+                                .where((element) => element != emotion)
+                                .toList();
+                          });
+                        },
+                      ),
+                    ))
+                .toList(),
+          ),
+        ],
       ),
     );
   }
